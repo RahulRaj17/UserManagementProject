d37018b79f3c2e02731b8c57dbade4949277cb5f
Rahul Raj <rahulraj.off@gmail.com>
2024-01-02 16:01:59 +0530

    Initial config setup

---
+++ src/test/java/com/learning/usermanagementproject/UserManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.usermanagementproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
